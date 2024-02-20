e1e5d25bcd99ee86a28911e4119a807e5d6871b6
nbkhope <ryumemaru@yahoo.com>
2024-02-20 13:47:57 -0500

    Incluir navegacao de telas

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ArcoIris from './arco-iris/ArcoIris';
+import Home from './components/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="HOME">
+          <Stack.Screen name="ARCO_IRIS" component={ArcoIris} options={{ title: 'Arco Iris' }} />
+          <Stack.Screen name="HOME" component={Home} options={{ title: 'Pagina Principal' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -13,8 +24,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
   },
 });
